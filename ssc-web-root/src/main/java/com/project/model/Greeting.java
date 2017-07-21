e92db0d611904c858b73ec46c5fc8f6646ce5b33
goforit <195302>
2017-07-21 08:59:48 +0800

    add hello

---
+++ ssc-web-root/src/main/java/com/project/model/Greeting.java
@@ -0,0 +1,22 @@
+package com.project.model;
+
+/**
+ * Created by goforit on 2017/7/21.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
