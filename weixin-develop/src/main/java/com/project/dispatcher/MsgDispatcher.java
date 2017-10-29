86c1ada6e07329077ba70133ceae9d35aaf9b7c0
goforit <195302>
2017-10-29 22:13:32 +0800

    测试

--- weixin-develop/src/main/java/com/project/dispatcher/MsgDispatcher.java
+++ weixin-develop/src/main/java/com/project/dispatcher/MsgDispatcher.java
@@ -1,15 +1,8 @@
 package com.project.dispatcher;
 
 import com.project.common.util.LogUtil;
-import com.project.model.message.response.Article;
-import com.project.model.message.response.NewsMessage;
-import com.project.model.message.response.TextMessage;
-import com.project.service.message.util.MessageUtil;
 import org.slf4j.Logger;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,55 +15,62 @@ public class MsgDispatcher {
     public static String processMessage(Map<String, String> map) {
         String openid = map.get("FromUserName"); //用户 openid
         String mpid = map.get("ToUserName");   //公众号原始 ID
-        if (map.get("MsgType").equals(MessageUtil.REQ_MESSAGE_TYPE_TEXT)) { // 文本消息
-            logger.info("==============这是文本消息！");
-            //普通文本消息
-            TextMessage txtmsg = new TextMessage();
-            txtmsg.setToUserName(openid);
-            txtmsg.setFromUserName(mpid);
-            txtmsg.setCreateTime(new Date().getTime());
-            txtmsg.setMsgType(MessageUtil.RESP_MESSAGE_TYPE_TEXT);
-
-            txtmsg.setContent("你好，这里是goforit测试账号！");
-            return MessageUtil.textMessageToXml(txtmsg);
-        }
-
-        if (map.get("MsgType").equals(MessageUtil.REQ_MESSAGE_TYPE_IMAGE)) { // 图片消息
-            logger.info("==============这是图片消息！");
-            NewsMessage newmsg = new NewsMessage();
-            newmsg.setToUserName(openid);
-            newmsg.setFromUserName(mpid);
-            newmsg.setCreateTime(new Date().getTime());
-            newmsg.setMsgType(MessageUtil.RESP_MESSAGE_TYPE_NEWS);
-
-            Article article = new Article();
-            article.setDescription("这是图文消息 1"); //图文消息的描述
-            article.setPicUrl("http://res.cuiyongzhi.com/2016/03/201603086749_6850.png"); //图文消息图片地址
-            article.setTitle("图文消息 1");  //图文消息标题
-            article.setUrl("http://www.cuiyongzhi.com");  //图文 url 链接
-            List<Article> list = new ArrayList<Article>();
-            list.add(article);     //这里发送的是单图文，如果需要发送多图文则在这里 list 中加入多个 Article 即可！
-            newmsg.setArticleCount(list.size());
-            newmsg.setArticles(list);
-            return MessageUtil.newsMessageToXml(newmsg);
-        }
-
-        if (map.get("MsgType").equals(MessageUtil.REQ_MESSAGE_TYPE_LINK)) { // 链接消息
-            logger.info("==============这是链接消息！");
-        }
-
-        if (map.get("MsgType").equals(MessageUtil.REQ_MESSAGE_TYPE_LOCATION)) { // 位置消息
-            logger.info("==============这是位置消息！");
-        }
-
-        if (map.get("MsgType").equals(MessageUtil.REQ_MESSAGE_TYPE_VIDEO)) { // 视频消息
-            logger.info("==============这是视频消息！");
-        }
-
-        if (map.get("MsgType").equals(MessageUtil.REQ_MESSAGE_TYPE_VOICE)) { // 语音消息
-            logger.info("==============这是语音消息！");
-        }
-
-        return null;
+        return "<xml>\n" +
+                "<ToUserName><![CDATA[toUser]]></ToUserName>\n" +
+                "<FromUserName><![CDATA[fromUser]]></FromUserName>\n" +
+                "<CreateTime>12345678</CreateTime>\n" +
+                "<MsgType><![CDATA[text]]></MsgType>\n" +
+                "<Content><![CDATA[你好]]></Content>\n" +
+                "</xml>";
+//        if (map.get("MsgType").equals(MessageUtil.REQ_MESSAGE_TYPE_TEXT)) { // 文本消息
+//            logger.info("文本消息");
+//            //普通文本消息
+//            TextMessage txtmsg = new TextMessage();
+//            txtmsg.setToUserName(openid);
+//            txtmsg.setFromUserName(mpid);
+//            txtmsg.setCreateTime(new Date().getTime());
+//            txtmsg.setMsgType(MessageUtil.RESP_MESSAGE_TYPE_TEXT);
+//
+//            txtmsg.setContent("你好，这里是goforit测试账号！");
+//            return MessageUtil.textMessageToXml(txtmsg);
+//        }
+//
+//        if (map.get("MsgType").equals(MessageUtil.REQ_MESSAGE_TYPE_IMAGE)) { // 图片消息
+//            logger.info("这是图片消息");
+//            NewsMessage newmsg = new NewsMessage();
+//            newmsg.setToUserName(openid);
+//            newmsg.setFromUserName(mpid);
+//            newmsg.setCreateTime(new Date().getTime());
+//            newmsg.setMsgType(MessageUtil.RESP_MESSAGE_TYPE_NEWS);
+//
+//            Article article = new Article();
+//            article.setDescription("这是图文消息 1"); //图文消息的描述
+//            article.setPicUrl("http://res.cuiyongzhi.com/2016/03/201603086749_6850.png"); //图文消息图片地址
+//            article.setTitle("图文消息 1");  //图文消息标题
+//            article.setUrl("http://www.cuiyongzhi.com");  //图文 url 链接
+//            List<Article> list = new ArrayList<Article>();
+//            list.add(article);     //这里发送的是单图文，如果需要发送多图文则在这里 list 中加入多个 Article 即可！
+//            newmsg.setArticleCount(list.size());
+//            newmsg.setArticles(list);
+//            return MessageUtil.newsMessageToXml(newmsg);
+//        }
+//
+//        if (map.get("MsgType").equals(MessageUtil.REQ_MESSAGE_TYPE_LINK)) { // 链接消息
+//            logger.info("这是链接消息！");
+//        }
+//
+//        if (map.get("MsgType").equals(MessageUtil.REQ_MESSAGE_TYPE_LOCATION)) { // 位置消息
+//            logger.info("这是位置消息！");
+//        }
+//
+//        if (map.get("MsgType").equals(MessageUtil.REQ_MESSAGE_TYPE_VIDEO)) { // 视频消息
+//            logger.info("这是视频消息！");
+//        }
+//
+//        if (map.get("MsgType").equals(MessageUtil.REQ_MESSAGE_TYPE_VOICE)) { // 语音消息
+//            logger.info("这是语音消息！");
+//        }
+//
+//        return null;
     }
 }
