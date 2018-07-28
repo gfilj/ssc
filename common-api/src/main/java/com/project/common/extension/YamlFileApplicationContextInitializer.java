a7aa98615705dbd67454f5b085e725d3e0c28f02
goforit <195302>
2018-07-28 10:39:40 +0800

    配置修改

---
+++ common-api/src/main/java/com/project/common/extension/YamlFileApplicationContextInitializer.java
@@ -0,0 +1,30 @@
+package com.project.common.extension;
+
+import com.project.common.util.LogUtil;
+import org.apache.commons.logging.Log;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
+
+/**
+ * author:luchangqing
+ * desc:配置导入类
+ */
+public class YamlFileApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    Log logger = LogUtil.getLogger(getClass());
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        try {
+            Resource resource = applicationContext.getResource("classpath:config/projectConfig.yml");
+            YamlPropertySourceLoader sourceLoader = new YamlPropertySourceLoader();
+            PropertySource<?> yamlTestProperties = sourceLoader.load("projectConfig", resource, null);
+            applicationContext.getEnvironment().getPropertySources().addFirst(yamlTestProperties);
+        } catch (Throwable e) {
+//            throw new RuntimeException(e);
+            logger.error(LogUtil.logstr("解析配置文件出错", "classpath:config/projectConfig.yml", "配置文件不存在"));
+        }
+    }
+}
